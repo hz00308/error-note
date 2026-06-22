003c4a34421642f7ee2aa84acc545ef8ee2f9a33
dltnalss <leesumin787@gmail.com>
2026-06-22 16:31:03 +0900

    에러 조회 및 삭제 기능 mapper 구현

--- error-note-backend/src/main/java/org/scoula/error_note/mapper/ErrorMapper.java
+++ error-note-backend/src/main/java/org/scoula/error_note/mapper/ErrorMapper.java
@@ -1,4 +1,17 @@
 package org.scoula.error_note.mapper;
 
+import org.scoula.error_note.domain.ErrorVO;
+
+import java.util.List;
+
 public interface ErrorMapper {
+
+    // 에러 전체 조회
+    List<ErrorVO> getErrorList();
+
+    // 에러 카테고리별 조회
+    List<ErrorVO> getErrorListByCategory(int cid);
+
+    // 에러 삭제
+    int deleteError(int eid);
 }
