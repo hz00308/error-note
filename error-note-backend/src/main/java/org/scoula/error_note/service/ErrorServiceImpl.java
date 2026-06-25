b58d229af240ed8b4e691551285e103b88e4274f
dltnalss <leesumin787@gmail.com>
2026-06-25 17:35:14 +0900

    @ 수정

--- error-note-backend/src/main/java/org/scoula/error_note/service/ErrorServiceImpl.java
+++ error-note-backend/src/main/java/org/scoula/error_note/service/ErrorServiceImpl.java
@@ -1,16 +1,26 @@
 package org.scoula.error_note.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.scoula.error_note.dto.ErrorDTO;
+import org.scoula.error_note.mapper.ErrorMapper;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service // 스프링 시작할 때 스캔해서 싱글톤으로 만들어줌.
+@RequiredArgsConstructor //생성자호출할 때 Autowired해줌.
+@Log4j2
 public class ErrorServiceImpl implements ErrorService{
-    @Override
+
+    private final ErrorMapper mapper;
+
+    @Override // s
     public List<ErrorDTO> getErrorList() {
         return List.of();
     }
 
-    @Override
+    @Override // s
     public List<ErrorDTO> getErrorListByCategory(int cid) {
         return List.of();
     }
@@ -30,7 +40,7 @@ public class ErrorServiceImpl implements ErrorService{
         return null;
     }
 
-    @Override
+    @Override //s
     public ErrorDTO deleteError(int eid) {
         return null;
     }
