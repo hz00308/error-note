0682c35b6259f529bc3e89ca3fd717271eca8a2b
hz00308 <kanghyeonji00308@gmail.com>
2026-06-29 17:46:00 +0900

    Error Controller 일부분 구현 및 테스트 완료

--- error-note-backend/src/main/java/org/scoula/error_note/controller/ErrorController.java
+++ error-note-backend/src/main/java/org/scoula/error_note/controller/ErrorController.java
@@ -2,9 +2,10 @@ package org.scoula.error_note.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.scoula.error_note.dto.ErrorDTO;
 import org.scoula.error_note.service.ErrorService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/errors")
@@ -21,12 +22,26 @@ public class ErrorController {
 
 
     // 에러 상세 조회
+    @GetMapping("/{eid}")
+    public ResponseEntity<ErrorDTO> getError(@PathVariable int eid) {
+        return ResponseEntity.ok(service.getError(eid));
+    }
 
 
     // 에러 등록
+    @PostMapping("")
+    public ResponseEntity<ErrorDTO> createError(@RequestBody ErrorDTO error) {
+        return ResponseEntity.ok(service.createError(error));
+    }
 
 
     // 에러 수정
+    @PutMapping("/{eid}")
+    public ResponseEntity<ErrorDTO> updateErrpr(@PathVariable int eid,
+                                                @RequestBody ErrorDTO error) {
+        error.setErrorId(eid);
+        return ResponseEntity.ok(service.updateError(error));
+    }
 
 
     // 에러 삭제
