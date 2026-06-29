ae346b1dd651353fbaaa75ca9168bf3c48d0f92a
dltnalss <leesumin787@gmail.com>
2026-06-29 17:46:37 +0900

    errorcontroller 구현 완료

--- error-note-backend/src/main/java/org/scoula/error_note/controller/ErrorController.java
+++ error-note-backend/src/main/java/org/scoula/error_note/controller/ErrorController.java
@@ -2,9 +2,12 @@ package org.scoula.error_note.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.scoula.error_note.dto.ErrorDTO;
 import org.scoula.error_note.service.ErrorService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/errors")
@@ -14,10 +17,15 @@ public class ErrorController {
 
     private final ErrorService service;
 
-    // 에러 전체 조회
 
-
-    // 에러 카테고리별 조회
+    // 에러 전체 & 카테고리별 조회 수민
+    @GetMapping("")
+    public ResponseEntity<List<ErrorDTO>> getErrorListByCategory(@RequestParam(required = false) Integer cid){
+                if (cid == null){
+                 return ResponseEntity.ok(service.getErrorList());
+        } else
+               return ResponseEntity.ok(service.getErrorListByCategory(cid));
+    }
 
 
     // 에러 상세 조회
@@ -29,7 +37,10 @@ public class ErrorController {
     // 에러 수정
 
 
-    // 에러 삭제
-
+//     에러 삭제 수민
+    @DeleteMapping("/{eid}")
+    public ResponseEntity<ErrorDTO> deleteError(@PathVariable int eid){
+        return ResponseEntity.ok(service.deleteError(eid));
+    }
 
 }
